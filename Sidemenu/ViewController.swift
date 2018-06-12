b3028c7a8c11e8015de37e460aa98659d2c17cd2
Atsushi Jike <a_jike@yumemi.co.jp>
2018-06-12 12:31:32 +0900

    Initial Commit

---
+++ Sidemenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sidemenu
+//
+//  Created by 寺家 篤史 on 2018/06/12.
+//  Copyright © 2018年 Yumemi Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
